b38f6a484a42345b5e0d699a36a379c30aca23d9
Toby Stimpson <tobystimpson@mail.com>
2019-03-06 15:12:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function yourAnimal() {
 
 function add2(n) {
   const two = 2
-  return parseInt(n, 10) + two //takes string and changes to integer 
+  return parseInt(n) + two //takes string and changes to integer 
 
   // Feel free to move things around!
 }
